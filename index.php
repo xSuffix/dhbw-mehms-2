1d50c102dea5029e5ff1b23fe3fb7efc5f93d739
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-12-16 02:13:22 +0100

    Add basic frontend for Mehm sorting

--- index.php
+++ index.php
@@ -10,26 +10,44 @@
 
 <body>
   <?php include("includes/header.php"); ?>
+  <?php
+      $sortOptions = array(
+        array("value" => "date", "name" => "Date"),
+        array("value" => "likes", "name" => "Likes"),
+        array("value" => "comments", "name" => "Comments"),
+      );
+
+      $search = isset($_GET['search']) ? $_GET["search"] : "";
+      $sort = isset($_GET['sort']) ? $_GET["sort"] : "date";
+      $desc = isset($_GET['desc']) ? $_GET["desc"] : false;
+  ?>
 
   <main class="container">
 
-    <div class="toolbar-wrapper">
-      <div class="toolbar">
+  <form id="query" name="query" method="GET" class="toolbar-wrapper">
+      <div class="searchbar">
         <svg xmlns="http://www.w3.org/2000/svg" class="i-search" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
         </svg>
-        <form name="search" method="GET">
-          <input name="search" type="text" spellcheck="false" autocomplete="off" placeholder="Search Mehms">
-        </form>
+        <input name="search" type="text" spellcheck="false" autocomplete="off" placeholder="Search Mehms" value="<?php echo $search ?>">
+
       </div>
-    </div>
+      <div class="sortbar">
+        <select name="sort" id="sort" onchange="document.getElementById('query').submit()">
+          <?php
+            for ($i = 0; $i < count($sortOptions); $i++) {
+              echo '<option value="' . $sortOptions[$i]["value"] . '" ' . ($sortOptions[$i]["value"] == $sort ? 'selected ' : '') . '>' . $sortOptions[$i]["name"] . '</option>';
+            }
+          ?>
+        </select>
+        <label for="desc">desc</label>
+        <input type="checkbox" id="desc" name="desc" <?php echo ' value="' . ($desc ? 'true" checked' : 'false"'); ?> onchange="document.getElementById('query').submit()">
+      </div>
+</form>
 
 
     <div id="mehm-gallery">
-      <?php $search = "";
-      if (isset($_GET['search'])) {
-        $search = $_GET['search'];
-      }
+      <?php 
 
       function ignoreCase($str)
       {
--- vines.php
+++ vines.php
@@ -8,7 +8,7 @@
   <style>
     :root {
       --banner-top: #ff9900;
-			--banner-bottom: #eb7967;
+      --banner-bottom: #eb7967;
     }
 
     .vines {
@@ -21,60 +21,56 @@
 <body>
   <?php include("includes/header.php"); ?>
   <main class="container">
-  <div class="heading">
-    <h1>Vines</h1>
-    <h2>Videos, Audio und Animationen</h2>
-  </div>
-  <div class="paper">
-    <h3>Vines</h3>
-    <hr>
-    <div class="gallery">
-      <?php
-
-        $dirname = "./assets/vines/";
-        $videos = glob($dirname . "*" . "*");
+    <div class="heading">
+      <h1>Vines</h1>
+      <h2>Videos, Audio und Animationen</h2>
+    </div>
+    <div class="paper">
+      <h3>Vines</h3>
+      <hr>
+      <div class="gallery">
+        <?php
+        $videos = ["edoxdRs6haA", "IdoD2147Fik", "7eKv4BEujFU", "gtnehMIdLMM", "oRMJ-UR9_Dk", "Rk1MYMPDx3s", "L9AJeakCa6w"];
 
         foreach ($videos as $video) {
-          $videoName = explode("/", $video);
-          $videoName = end($videoName);
           try {
-            echo '<figure class="vine"><video class="vine" width="560" height="315" controls><source src="' . $video . '" type="video/mp4">Your browser does not support the video tag.
-            </video><figcaption>' . $videoName . '</figcaption>
-            </figure>';
+            echo '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' . $video . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
           } catch (DivisionByZeroError $e) {
           }
-          }
-      ?>
-    </div>
-    <hr>
-    <h3>External and Audio-only</h3>
-    <hr>
-    <div class="gallery">
-    <figure class="vine">
-    <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/EqekSuj5HCo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <figcaption>Walter Frosch - Vollblutprofi</figcaption>
-    </figure>
-    <figure class="vine">
-    <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/asjQNZn7vng" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <figcaption>Rendering at 5 AM</figcaption>
-    </figure>
-    <figure class="vine">
-    <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/b8HO6hba9ZE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <figcaption><em>We like to Party</em> - Thomas the Dank Engine</figcaption>
-    </figure>
-    <figure class="vine">
-    <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/nXjro36-uLE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <figcaption>Alex Jones vs Creeper</figcaption>
-    </figure>
-    <figure class="vine">
-    <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/DRZ3F5VqseU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <figcaption>Shhh... Melon Thief</figcaption>
-    </figure>
-    <figure class ="vine">
-      <audio class="vine" controls><source src="assets/audio/audio.mp3" type="audio/mp3"></audio>
-      <figcaption>Rick Astley - Never gonna give you up</figcaption>
-    </figure>
-    </div>
+        }
+        ?>
+      </div>
+      <hr>
+      <h3>External and Audio-only</h3>
+      <hr>
+      <div class="gallery">
+        <figure class="vine">
+          <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/EqekSuj5HCo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption>Walter Frosch - Vollblutprofi</figcaption>
+        </figure>
+        <figure class="vine">
+          <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/asjQNZn7vng" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption>Rendering at 5 AM</figcaption>
+        </figure>
+        <figure class="vine">
+          <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/b8HO6hba9ZE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption><em>We like to Party</em> - Thomas the Dank Engine</figcaption>
+        </figure>
+        <figure class="vine">
+          <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/nXjro36-uLE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption>Alex Jones vs Creeper</figcaption>
+        </figure>
+        <figure class="vine">
+          <iframe class="vine" width="560" height="315" src="https://www.youtube.com/embed/DRZ3F5VqseU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption>Shhh... Melon Thief</figcaption>
+        </figure>
+        <figure class="vine">
+          <audio class="vine" controls>
+            <source src="assets/audio/audio.mp3" type="audio/mp3">
+          </audio>
+          <figcaption>Rick Astley - Never gonna give you up</figcaption>
+        </figure>
+      </div>
     </div>
   </main>
 
