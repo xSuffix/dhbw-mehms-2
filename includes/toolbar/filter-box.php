c9069c7edd9d1f76a940e980f10b1751ba32b3dd
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-12-23 00:51:26 +0100

    Remove useless label

--- includes/toolbar/filter-box.php
+++ includes/toolbar/filter-box.php
@@ -1,15 +1,15 @@
 <?php $filterOptions = array(
-    array("value" => "", "name" => "Alle"),
-    array("value" => "Programmieren", "name" => "Programmieren"),
-    array("value" => "DHBW", "name" => "DHBW"),
-    array("value" => "Andere", "name" => "Andere")
+  array("value" => "", "name" => "Alle"),
+  array("value" => "Programmieren", "name" => "Programmieren"),
+  array("value" => "DHBW", "name" => "DHBW"),
+  array("value" => "Andere", "name" => "Andere")
 );
 $category = $_GET["filter"] ?? "";
 
 ?>
 
-<label for="filter"></label><select name="filter" id="filter" class="box" onchange="document.getElementById('query').submit()">
-    <?php for ($i = 0; $i < count($filterOptions); $i++) {
-        echo '<option value="' . $filterOptions[$i]["value"] . '" ' . ($filterOptions[$i]["value"] == $category ? 'selected ' : '') . '>' . $filterOptions[$i]["name"] . '</option>';
-    } ?>
+<select name="filter" id="filter" class="box" onchange="document.getElementById('query').submit()">
+  <?php for ($i = 0; $i < count($filterOptions); $i++) {
+    echo '<option value="' . $filterOptions[$i]["value"] . '" ' . ($filterOptions[$i]["value"] == $category ? 'selected ' : '') . '>' . $filterOptions[$i]["name"] . '</option>';
+  } ?>
 </select>
