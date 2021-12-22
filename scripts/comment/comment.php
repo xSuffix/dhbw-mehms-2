b46c1501ed84f3ac59715d2d18d1c56eb41788fd
nillga <71821765+nillga@users.noreply.github.com>
2021-12-23 00:34:02 +0100

    weird error!

--- scripts/comment/comment.php
+++ scripts/comment/comment.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Database.php';
+require_once '../Database.php';
 $db = new Database();
 
 $userId = $_POST["user"];
---
+++ scripts/comment/deleteComment.php
@@ -0,0 +1,7 @@
+<?php
+    require_once '../Database.php';
+    $db = new Database();
+
+    $id = $_POST['id'];
+    $db->database->query("DELETE FROM comments WHERE ID = " .$id);
+?>
