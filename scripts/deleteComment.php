4cb34a88ecd056aa06777bba16bf88eef01f9795
nillga <71821765+nillga@users.noreply.github.com>
2021-12-22 23:58:31 +0100

    comments are deletable

--- scripts/deleteComment.php
+++ scripts/deleteComment.php
@@ -1,3 +1,12 @@
-$db->database->query("DELETE comments WHERE ID = '$index");
+// $db->database->query("DELETE comments WHERE ID = '$index");
 
-$db->database->query("UPDATE comments SET Comment = '$text' WHERE ID = '$index'");
+// $db->database->query("UPDATE comments SET Comment = '$text' WHERE ID = '$index'");
+
+<?php
+    require_once 'Database.php';
+    $db = new Database();
+    // Entscheidet anhand eingegangener Action, ob es sich um approve oder decline handelt
+    print_r($_POST);
+    $id = $_POST['id'];
+    $db->database->query("DELETE FROM comments WHERE ID = " .$id);
+?>
