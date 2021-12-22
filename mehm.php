415d4520cf3bf55f60cdb11c8a749f065d89ad59
Madokami <fabianweller7@gmail.com>
2021-12-22 16:48:27 +0100

    Das kann ich aber

--- mehm.php
+++ mehm.php
@@ -79,10 +79,11 @@
    * @param boolean $admin if hidden Mehms should be found
    */
   function getMehm(int $id, bool $admin): array {
-    $query = 'SELECT *, count(l.MehmID) AS likeCount, mehms.Type AS Type, mehms.ID AS ID
+    $query = 'SELECT *, count(l.MehmID) AS likeCount, count(c.MehmID) AS commentCount, mehms.Type AS Type, mehms.ID AS ID
       FROM mehms
-      LEFT JOIN Users u ON mehms.UserID = u.ID
-      LEFT JOIN likes l on mehms.ID = l.MehmID
+      LEFT JOIN users u ON mehms.UserID = u.ID
+      LEFT JOIN likes l ON mehms.ID = l.MehmID
+      LEFT JOIN comments c ON mehms.ID = c.MehmID
       WHERE mehms.ID =' . $id;
 
     if (!$admin) {
@@ -223,7 +224,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="icon-md" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
             </svg>
-            x Kommentare
+              <?php echo $mehm['commentCount'] ?> Kommentar<?php if ($mehm['commentCount'] != 1) echo "e"; ?>
           </a>
           <span class="meta-icon icon-text" onclick="copyURL()">
             <svg xmlns="http://www.w3.org/2000/svg" id="share-button" class="icon-md" viewBox="0 0 20 20" fill="currentColor">
