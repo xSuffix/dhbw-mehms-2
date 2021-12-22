06c0499c663250cf78f0037065b74b59195639a6
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-12-22 16:18:57 +0100

    Add share button

--- mehm.php
+++ mehm.php
@@ -56,6 +56,16 @@
         });
       });
     });
+
+    function copyURL() {
+      const copy = document.createElement("input"), url = window.location.href;
+      document.body.appendChild(copy);
+      copy.value = url.split("#")[0];
+      copy.select();
+      document.execCommand("copy");
+      document.body.removeChild(copy);
+      document.getElementById("share-button").classList.add("pressed");
+    }
   </script>
   <?php
   require_once 'scripts/Database.php';
@@ -199,17 +209,23 @@
               $liked = $db->database->query($query)->fetchall();
               if (!empty($liked)) $beat = ' beat';
             }
-            echo '<svg xmlns="http://www.w3.org/2000/svg" class="i-heart' . $beat . '" id="' . $mehm['ID'] . ' ' . $sess . '" viewBox="0 0 20 20" fill="currentColor">
+            echo '<svg xmlns="http://www.w3.org/2000/svg" class="icon-md' . $beat . '" id="' . $mehm['ID'] . ' ' . $sess . '" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
               </svg>' ?>
             <?php echo $mehm['likeCount'] ?> Like<?php if ($mehm['likeCount'] != 1) echo "s"; ?>
           </button>
           <a class="meta-icon" href="#comments">
-            <svg xmlns="http://www.w3.org/2000/svg" class="i-comment" viewBox="0 0 20 20" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon-md" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
             </svg>
             x Kommentare
           </a>
+          <span class="meta-icon" onclick="copyURL()">
+            <svg xmlns="http://www.w3.org/2000/svg" id="share-button" class="icon-md" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+            </svg>
+            Teilen
+          </span>
         </div>
       </aside>
     </div>
