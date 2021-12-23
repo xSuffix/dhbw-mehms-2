4f89154fda109273e9f40ed29e9793d1c6354988
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-12-23 13:31:58 +0100

    Fix Mehm upload

--- php/mehm-formular.php
+++ php/mehm-formular.php
@@ -5,61 +5,62 @@ Utils::checkLogin(true);
 
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
-    <link href="../css/formular.css" rel="stylesheet">
-    <meta charset="UTF-8">
-    <title> Dein Mehm - Upload </title>
-    <?php include("includes/meta.php"); ?>
-    <style>
-        :root {
-            --banner-top: #b167eb;
-            --banner-bottom: #4bd8f6;
-        }
-    </style>
+  <link href="../css/formular.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <title> Dein Mehm - Upload </title>
+  <?php include("includes/meta.php"); ?>
+  <style>
+    :root {
+      --banner-top: #b167eb;
+      --banner-bottom: #4bd8f6;
+    }
+  </style>
 </head>
 
 <body>
-<?php include("includes/header.php"); ?>
-<main class="container">
+  <?php include("includes/header.php"); ?>
+  <main class="container">
     <section class="paper">
-        <?php
-        require_once 'scripts/Database.php';
-        $db = new Database();
+      <?php
+      require_once 'scripts/Database.php';
+      $db = new Database();
 
-        // Verschiebe das hochgeladene Mehm in den assets/mehm/ Ordner.
-        $target_dir = "assets/mehms/";
-        $target_file = $target_dir . basename($_FILES["mDatei"]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+      // Verschiebe das hochgeladene Mehm in den assets/mehm/ Ordner.
+      $target_dir = "../assets/mehms/";
+      $target_file = $target_dir . basename($_FILES["mDatei"]["name"]);
+      $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-        // Prüfe, ob Dateiformat erlaubt ist.
-        if ($imageFileType == 'png' || $imageFileType == 'jpg' || $imageFileType == 'jpeg' || $imageFileType == 'gif' || $imageFileType == 'webp') {
+      // Prüfe, ob Dateiformat erlaubt ist.
+      if ($imageFileType == 'png' || $imageFileType == 'jpg' || $imageFileType == 'jpeg' || $imageFileType == 'gif' || $imageFileType == 'webp') {
 
-            if (move_uploaded_file($_FILES["mDatei"]["tmp_name"], $target_file)) {
-                // Hole notwendige Parameter aus POST und schreibe die Informationen in die Datenbank.
-                $desc = $_POST["mBildbeschreibung"] == '' ? NULL : $_POST["mBildbeschreibung"];
-                $filename = $_FILES["mDatei"]["name"];
-                $kategorie = $_POST["mKategorie"];
-                $title = $_POST["mTitel"];
-                $userID = $_SESSION['id'];
-                $result = $db->database->query("INSERT INTO mehms (Path, Title, UserID, Type, Description) VALUES (
+        if (move_uploaded_file($_FILES["mDatei"]["tmp_name"], $target_file)) {
+          // Hole notwendige Parameter aus POST und schreibe die Informationen in die Datenbank.
+          $desc = $_POST["mBildbeschreibung"] == '' ? NULL : $_POST["mBildbeschreibung"];
+          $filename = $_FILES["mDatei"]["name"];
+          $kategorie = $_POST["mKategorie"];
+          $title = $_POST["mTitel"];
+          $userID = $_SESSION['id'];
+          $result = $db->database->query("INSERT INTO mehms (Path, Title, UserID, Type, Description) VALUES (
                     '$filename',
                     '$title',
                     '$userID',
                     '$kategorie',
                     '$desc');");
 
-                echo "<h1> Viele Dank für deine Einsendung der Kategorie " . $_POST["mKategorie"] . "!</h1> <br><p class=\"response\"> Die Datei " . htmlspecialchars(basename($_FILES["mDatei"]["name"])) . " wurde erfolgreich hochgeladen. </p>";
-
-            } else {
-                echo "<h1> Tut uns Leid, es ist ein Fehler aufgetreten. Bitte versuche es erneut!</h1>";
-            }
+          echo "<h1> Viele Dank für deine Einsendung der Kategorie " . $_POST["mKategorie"] . "!</h1> <br><p class=\"response\"> Die Datei " . htmlspecialchars(basename($_FILES["mDatei"]["name"])) . " wurde erfolgreich hochgeladen. </p>";
         } else {
-            echo "<h1>Falsches Dateiformat! Bitte benutze nur .png .jpg .jpeg .gif oder .webp</h1>";
+          echo "<h1> Tut uns Leid, es ist ein Fehler aufgetreten. Bitte versuche es erneut!</h1>";
         }
-        ?>
+      } else {
+        echo "<h1>Falsches Dateiformat! Bitte benutze nur .png .jpg .jpeg .gif oder .webp</h1>";
+      }
+      ?>
     </section>
-</main>
-<?php include("includes/footer.php"); ?>
-<?php include("includes/bottom-navigation.php"); ?>
+  </main>
+  <?php include("includes/footer.php"); ?>
+  <?php include("includes/bottom-navigation.php"); ?>
 </body>
+
 </html>
